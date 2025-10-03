3ada738a36588dfcacb75b40e4545fa4b536f74a
stvishnukmr <zvishnu21z@gmail.com>
2025-10-03 14:59:26 +0530

    vishnu

--- src/test/java/com/app/functional/GlobalProperties.java
+++ src/test/java/com/app/functional/GlobalProperties.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import io.restassured.response.Response;
 
 public class GlobalProperties {
+	//globalpropgithub
 	
 	private static Map<String, String> globalheaders= new HashMap<String, String>();
 	private static Response response=null;
