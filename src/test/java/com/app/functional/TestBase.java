0af453aa00c772881bc73ec0721c8989d50cc400
Vishnu Kumar <zvishnu21z@gmail.com>
2025-10-21 19:47:59 +0530

    Add comment for testing in TestBase.java
    Added a comment for testing purposes.

--- src/test/java/com/app/functional/TestBase.java
+++ src/test/java/com/app/functional/TestBase.java
@@ -3,7 +3,7 @@ package com.app.functional;
 import io.restassured.builder.RequestSpecBuilder;
 
 public class TestBase {
-	
+	//for testing git
 	public RequestSpecBuilder reqspecbiulder = new RequestSpecBuilder();
 
 	public void handleproxy(){
